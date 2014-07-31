7807670d03ecc74ed7a977e48620094a198f775d
hongbing <hongbing@staff.sina.com.cn>
2014-07-31 16:33:44 +0800

    0.0.2

---
+++ ddal/src/main/java/com/weibo/newbie/common/DateUtil.java
@@ -0,0 +1,22 @@
+package com.weibo.newbie.common;
+
+import java.util.Calendar;
+
+public class DateUtil {
+
+	/**
+	 * get month like:201407
+	 * @return
+	 */
+	public static String getCurrentMonth() {
+		Calendar cal = Calendar.getInstance();
+		Integer year = cal.get(Calendar.YEAR);
+		Integer month = cal.get(Calendar.MONTH) + 1;
+		return year.toString() + (month >= 10 ? month.toString() : ("0" + month));
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(DateUtil.getCurrentMonth());
+	}
+}
